050c5a4623e71377db5b1066c3c3598a625473e7
ekapratama93 <ekapratama93@gmail.com>
2019-04-05 17:06:37 +0700

    add result storage

---
+++ tc_mongodb/result_storages/mongo_result_storage.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2015 Thumbor-Community
+
+import time
+from datetime import datetime, timedelta
+
+import gridfs
+from pymongo import MongoClient
+from thumbor.result_storages import BaseStorage
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
+class Storage(BaseStorage):
+
+    '''start_time is used to calculate the last modified value when an item
+    has no expiration date.
+    '''
+    start_time = None
+
+    def __init__(self, context):
+        self.database, self.storage = self.__conn__()
+
+        if not Storage.start_time:
+            Storage.start_time = time.time()
+
+    def __conn__(self):
+        connection = MongoClient(
+            self.context.config.MONGO_RESULT_STORAGE_SERVER_HOST,
+            self.context.config.MONGO_RESULT_STORAGE_SERVER_PORT
+        )
+
+        database = connection[self.context.config.MONGO_RESULT_STORAGE_SERVER_DB]
+        storage = database[
+            self.context.config.MONGO_RESULT_STORAGE_SERVER_COLLECTION
+        ]
+
+        return database, storage
+
+    def is_auto_webp(self):
+        '''
+        TODO This should be moved into the base storage class.
+             It is shared with file_result_storage
+        :return: If the file is a webp
+        :rettype: boolean
+        '''
+
+        return self.context.config.AUTO_WEBP \
+            and self.context.request.accepts_webp
+
+    def get_key_from_request(self):
+        '''Return a key for the current request url.
+        :return: The storage key for the current url
+        :rettype: string
+        '''
+
+        path = "result:%s" % self.context.request.url
+
+        if self.is_auto_webp():
+            path += '/webp'
+
+        return path
+
+    def get_max_age(self):
+        '''Return the TTL of the current request.
+        :returns: The TTL value for the current request.
+        :rtype: int
+        '''
+
+        default_ttl = self.context.config.RESULT_STORAGE_EXPIRATION_SECONDS
+        if self.context.request.max_age == 0:
+            return self.context.request.max_age
+
+        return default_ttl
+
+    def put(self, bytes):
+        '''Save to mongodb
+        :param bytes: Bytes to write to the storage.
+        :return: MongoDB _id for the current url
+        :rettype: string
+        '''
+        doc = {
+            'key': self.get_key_from_request,
+            'created_at': datetime.now()
+        }
+
+        file_doc = dict(doc)
+
+        fs = gridfs.GridFS(self.database)
+        file_data = fs.put(StringIO(bytes), **doc)
+
+        file_doc['file_id'] = file_data
+        self.database.storage.insert(file_doc)
+
+        return self.get_key_from_request
+
+    def get(self):
+        '''Get the item from MongoDB.'''
+
+        key = self.get_key_from_request
+        stored = next(self.storage.find({
+            'key': key,
+            'created_at': {'$gte': datetime.utcnow() - self.get_max_age},
+        }, {
+            'file_id': True,
+        }).limit(1), None)
+
+        if not stored:
+            return None
+
+        fs = gridfs.GridFS(self.database)
+
+        contents = fs.get(stored['file_id']).read()
+        return str(contents)
+
+    def last_updated(self):
+        '''Return the last_updated time of the current request item
+        :return: A DateTime object
+        :rettype: datetetime.datetime
+        '''
+
+        key = self.get_key_from_request
+        max_age = self.get_max_age
+
+        if max_age == 0:
+            return datetime.fromtimestamp(Storage.start_time)
+
+        image = next(self.storage.find({
+            'key': key,
+            'created_at': {'$gte': datetime.utcnow() - self.get_max_age},
+        }, {
+            'created_at': True, '_id': False
+        }).limit(1), None)
+
+        age = int((datetime.utcnow() - image['created_at']).total_seconds())
+        ttl = max_age - age
+
+        if max_age <= 0:
+            return datetime.fromtimestamp(Storage.start_time)
+
+        if ttl >= 0:
+            return datetime.utcnow() - timedelta(
+                seconds=(
+                    max_age - ttl
+                )
+            )
+
+        # Should never reach here. It means the storage put failed or the item
+        # somehow does not exists anymore
+        return datetime.utcnow()
